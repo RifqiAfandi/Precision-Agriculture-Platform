bfdb3b31cf0cac1b56b92cf1e5d4559d46f19401
Rifqi Afandi <146163545+RifqiAfandi@users.noreply.github.com>
2025-08-28 22:32:16 +0700

    feat: add landing page component and connect it in App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { LandingPage } from "./components/LandingPage";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default function App() {
+  const renderPage = () => {
+    switch (currentPage) {
+      case "landing":
+        return <LandingPage onNavigate={setCurrentPage} />;
+      default:
+        return <LandingPage onNavigate={setCurrentPage} />;
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-background">
+      {renderPage()}
+      <Toaster />
+    </div>
+  );
+}
